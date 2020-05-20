04cd2e0b59efacd6a767d5ec879fc7c4a7513764
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-20 01:12:24 -0300

    style: add style pokemon item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PokemonItem from './components/PokemonItem'
+import './App.css'
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setPokemons(...pokemons, data.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [pokemons])
+
+  // function searchPokemon(pokeUrl) {
+  //   fetch(pokeUrl)
+  //     .then((response) => {
+  //       return response.json()
+  //     })
+  //     .then((data) => {
+  //       // setPokemons(...pokemons, data)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     })
+  // }
+
   return (
     <div className="wrapper_app">
-
+      <div className="container">
+        <div className="wrapper_title">
+          <h1>Poke Api</h1>
+        </div>
+        <div className="wrapper_pokemons">
+          <div className="wrapper_subtitle">
+            <h3>Pokemons</h3>
+          </div>
+          <div className="row">
+            <PokemonItem />
+            <PokemonItem />
+            <PokemonItem />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/PokemonItem/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+const PokemonItem = () => {
+  return (
+    <div className="wrapper_pokemon_item col-12 col-sm-12 col-md-6 col-lg-6">
+      <div className="wrapper_card">
+        <div className="row">
+          <div className="col-6">
+            <p>Nome: Bubb</p>
+          </div>
+          <div className="col-6 text-right">
+            <a href="/">Ver mais</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default PokemonItem;
