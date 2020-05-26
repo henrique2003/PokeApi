ac9ea720e4d4f26a1c53359a4978c973c911793a
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-26 11:34:32 -0300

    feat: add AboutPokemon api

--- src/components/AboutPokemon/index.js
+++ src/components/AboutPokemon/index.js
@@ -1,11 +1,58 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa'
+import axios from 'axios'
 
 import './style.css'
 
-const AboutPokemon = () => {
+const AboutPokemon = ({ history, match }) => {
   const [changeSprite, setChangeSprite] = useState(false)
+  const [pokemon, setPokemon] = useState({
+    sprites: {
+      front_shiny: '',
+      front_default: ''
+    },
+    types: [{
+      type: {
+        name: ''
+      }
+    }],
+    abilities: [{
+      ability: {
+        name: ''
+      }
+    }],
+    name: '',
+    id: 0,
+    weight: 0,
+    base_experience: 0
+  })
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${match.params.id}`)
+
+        return setPokemon(res.data)
+      } catch (error) {
+        return history.push('/')
+      }
+    }
+
+    loadPokemon()
+  }, [history, match.params.id])
+
+  function showTypes() {
+    return pokemon.types.map(({ type }) => {
+      return type.name
+    }).join(', ')
+  }
+
+  function showAbilitys() {
+    return pokemon.abilities.map(({ ability }) => {
+      return ability.name
+    }).join(', ')
+  }
 
   return (
     <div className="wrapper_about_pokemon">
@@ -25,28 +72,28 @@ const AboutPokemon = () => {
             >
               <img
                 src={changeSprite ?
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png' :
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'}
+                  pokemon.sprites.front_shiny :
+                  pokemon.sprites.front_default}
                 alt="spites"
                 className="img-fluid"
               />
             </div>
-            <div className="col-12 col-sm-12 col-md-3 col-lg-2 mt-4 mt-sm-4 mt-md-0">
+            <div className="col-12 col-sm-12 col-md-4 col-lg-3 mt-4 mt-sm-4 mt-md-0">
               <div className="content">
-                <p>Nome: <span>ditto</span></p>
-                <p>Tipo: <span>normal</span></p>
-                <p>Peso: <span>40</span></p>
+                <p>Nome: <span>{pokemon.name}</span></p>
+                <p>Pokedéx: <span>{pokemon.id}</span></p>
+                <p>Peso: <span>{pokemon.weight}</span></p>
               </div>
             </div>
             <div className="col-12 col-sm-12 col-md-3 col-lg-3">
               <div className="content">
-                <p>Pokedéx: <span>132</span></p>
-                <p>Experiência base: <span>101xp</span></p>
+                <p>Experiência base: <span>{pokemon.base_experience}xp</span></p>
+                <p>Tipo: <span>{showTypes()}</span></p>
               </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-4 col-lg-5">
+            <div className="col-12 col-sm-12 col-md-3 col-lg-4">
               <div className="content">
-                <p>Abilidades: <span>imposte, limber</span></p>
+                <p>Abilidades: <span>{showAbilitys()}</span></p>
               </div>
             </div>
           </div>
@@ -56,4 +103,4 @@ const AboutPokemon = () => {
   )
 }
 
-export default AboutPokemon
+export default withRouter(AboutPokemon)
--- src/components/Home/PokemonItem/index.js
+++ src/components/Home/PokemonItem/index.js
@@ -1,8 +1,16 @@
 import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
+
 import './style.css'
 
 const PokemonItem = ({ pokemon }) => {
   const { url, name } = pokemon
+
+  function formatUrl() {
+    const newUrl = url.split("/")
+
+    return newUrl[6]
+  }
 
   return (
     <div className="wrapper_pokemon_item col-12 col-sm-12 col-md-6 col-lg-6">
@@ -12,7 +20,7 @@ const PokemonItem = ({ pokemon }) => {
             <p>Nome: <span>{name}</span></p>
           </div>
           <div className="col-6 text-right">
-            <a href={url}>Ver mais</a>
+            <Link to={`/sobre/${formatUrl(url)}`}>Ver mais</Link>
           </div>
         </div>
       </div>
